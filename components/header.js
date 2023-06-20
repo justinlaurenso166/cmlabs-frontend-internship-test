7a68fddd9ae378ee98354b58cb13c1021bceed7e
Justin Laurenso <justin.laurenso166@gmail.com>
2023-06-20 22:50:25 +0700

    Update header.js

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <header class="header">
-            <h1 class="font-bold text-xl sm:text-2xl tracking-wide hover:cursor-pointer" > <a href="/">MealApp</a></h1>
+            <h1 class="font-bold text-xl sm:text-2xl tracking-wide hover:cursor-pointer" > <a href="./../index.html">MealApp</a></h1>
             <div class="hidden md:block lg:block xl:block">
                 <nav class="flex gap-10 font-semibold text-lg">
                     <a href="../index.html">Home</a>
@@ -24,7 +24,7 @@ class Header extends HTMLElement {
         <div class="relative">
             <div class="z-50 w-full absolute hidden bg-white shadow-md" id="menu-dropdown">
                 <nav class="flex flex-col font-semibold text-lg">
-                    <a href="/" class="py-3 text-center hover:bg-black hover:text-white transition-all delay-100 ease-in">Home</a>
+                    <a href="./../index.html" class="py-3 text-center hover:bg-black hover:text-white transition-all delay-100 ease-in">Home</a>
                     <a href="" class="py-3 text-center hover:bg-black hover:text-white transition-all delay-100 ease-in">Foods</a>
                     <a href="" class="py-3 text-center hover:bg-black hover:text-white transition-all delay-100 ease-in">Ingredients</a>
                     <a href="" class="py-3 text-center hover:bg-black hover:text-white transition-all delay-100 ease-in">Local Culinary</a>
