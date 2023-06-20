26836aa21ad6a6fe5ab199e513f7c29778009bc1
Justin Laurenso <justin.laurenso166@gmail.com>
2023-06-20 22:42:50 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
     const categoryList = $('#categoryList');
-    const baseUrl = window.location.href.split('/').slice(0, -1).join('/');
-    console.log(baseUrl)
 
     $.ajax({
         url: 'https://www.themealdb.com/api/json/v1/1/categories.php',
@@ -10,7 +8,7 @@ $(document).ready(function () {
             const categories = response.categories;
             categories.forEach(category => {
                 const li = $('<li>').attr('class', 'category');
-                const image = $('<img>').attr('src', category.strCategoryThumb).attr('alt', category.strCategory).attr('class', 'category-image');
+                const image = $('<img>').attr('src', category.strCategoryThumb).attr('alt', category.strCategory).attr('class','category-image');
                 const dimmer = $('<div>').attr('class', 'dimmer');
                 const categoryName = $('<span>').text(category.strCategory).attr('class', 'category-name');
 
@@ -18,11 +16,9 @@ $(document).ready(function () {
                 categoryList.append(li);
 
                 li.click(function () {
-                    const baseUrl = window.location.href.split('/').slice(0, -1).join('/');
-                    window.location.href = baseUrl + '/pages/category_detail.html?category-name=' + category.strCategory;
+                    window.location.href = './pages/category_detail.html?category-name=' + category.strCategory;
+                    
                 });
-
-
             });
         },
         error: function (error) {
